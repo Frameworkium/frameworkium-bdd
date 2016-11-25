0ed24448609ff581fd5484c71048cfc9f86dea84
robertgates55 <robertgates55@gmail.com>
2016-11-25 12:11:49 +0000

    Using latest fwium-core (2.3.0)
    Still using per-feature runner.

--- src/test/java/com/google/glue/BrowserSetup.java
+++ src/test/java/com/google/glue/BrowserSetup.java
@@ -4,23 +4,25 @@ import com.frameworkium.core.ui.tests.BaseTest;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import org.testng.annotations.AfterTest;
 
 /**
  * Functionality for setup and teardown
  */
 public class BrowserSetup extends BaseTest {
 
-    Boolean driverExists = false;
-
     @Before
     public void setup() {
+        try {
+            BaseTest.getDriver().quit();
+        } catch (Exception e) {}
         // This needs to be run once at the very start of any scenario
         instantiateDriverObject();
         configureBrowserBeforeUse();
     }
 
-    @After
-    public void tearDown(Scenario scenario) {
+    @AfterTest
+    public void tearDown() {
         BaseTest.getDriver().quit();
     }
 
