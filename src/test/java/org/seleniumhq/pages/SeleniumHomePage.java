790071ae2e0d4bc6523921d697d1d31e2a963618
MJL33 <mjlee.1@gmail.com>
2021-01-07 01:15:49 +0800

    Fix selenium page hidden download link

--- src/test/java/org/seleniumhq/pages/SeleniumHomePage.java
+++ src/test/java/org/seleniumhq/pages/SeleniumHomePage.java
@@ -2,6 +2,7 @@ package org.seleniumhq.pages;
 
 import com.frameworkium.core.ui.pages.BasePage;
 import com.frameworkium.core.ui.pages.PageFactory;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import ru.yandex.qatools.htmlelements.element.Link;
 
@@ -10,6 +11,9 @@ public class SeleniumHomePage extends BasePage<SeleniumHomePage> {
     @FindBy(css = "#navbar [href='/downloads']")
     private Link downloadLink;
 
+    @FindBy(css = ".headerLink + #dropdownButton")
+    private WebElement menuLink;
+
     public static void open() {
         PageFactory.newInstance(
                 SeleniumHomePage.class,
@@ -17,6 +21,9 @@ public class SeleniumHomePage extends BasePage<SeleniumHomePage> {
     }
 
     public void gotoDownloadsPage() {
+        if (menuLink.isDisplayed()) {
+            menuLink.click();
+        }
         downloadLink.click();
     }
 }
