e3a2db92b1cb6e1a260f8661990154a7dce2a6a9
MJ <mjlee.1@gmail.com>
2021-09-25 21:15:28 +0800

    Update pages to use internal HtmlElements

--- src/test/java/com/google/pages/MapInfoPane.java
+++ src/test/java/com/google/pages/MapInfoPane.java
@@ -5,16 +5,13 @@ import com.frameworkium.core.ui.pages.BasePage;
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import ru.yandex.qatools.htmlelements.annotations.Name;
 
 public class MapInfoPane extends BasePage<MapInfoPane> {
 
     @Visible
-    @Name("Place information header")
     @FindBy(css = "#pane .section-hero-header-title")
     private WebElement infoHeader;
 
-    @Name("Place information address")
     @FindBy(css = "#pane .widget-pane-content [data-item-id='address']")
     private WebElement addressSpan;
 
--- src/test/java/com/google/pages/MapPage.java
+++ src/test/java/com/google/pages/MapPage.java
@@ -1,17 +1,16 @@
 package com.google.pages;
 
+import com.frameworkium.core.htmlelements.element.TextInput;
 import com.frameworkium.core.ui.annotations.Visible;
 import com.frameworkium.core.ui.pages.BasePage;
 import io.qameta.allure.Step;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.FindBy;
-import ru.yandex.qatools.htmlelements.annotations.Name;
-import ru.yandex.qatools.htmlelements.element.TextInput;
+
 
 public class MapPage extends BasePage<MapPage> {
 
     @Visible
-    @Name("Search Box")
     @FindBy(css = "input#searchboxinput")
     private TextInput searchBox;
 
