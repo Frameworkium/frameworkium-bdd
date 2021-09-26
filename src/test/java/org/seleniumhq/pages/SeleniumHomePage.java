2ffbb5162511cffb3ec6777d9e1f7bf5eb8f364d
MJL33 <mjlee.1@gmail.com>
2021-09-26 18:23:07 +0800

    Fix SeleniumHomePage to wait for downloadLink to be clickable

--- src/test/java/org/seleniumhq/pages/SeleniumHomePage.java
+++ src/test/java/org/seleniumhq/pages/SeleniumHomePage.java
@@ -1,10 +1,12 @@
 package org.seleniumhq.pages;
 
 import com.frameworkium.core.htmlelements.element.Link;
+import com.frameworkium.core.ui.UITestLifecycle;
 import com.frameworkium.core.ui.pages.BasePage;
 import com.frameworkium.core.ui.pages.PageFactory;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SeleniumHomePage extends BasePage<SeleniumHomePage> {
 
@@ -24,6 +26,7 @@ public class SeleniumHomePage extends BasePage<SeleniumHomePage> {
         if (menuLink.isDisplayed()) {
             menuLink.click();
         }
+        UITestLifecycle.get().getWait().until(ExpectedConditions.elementToBeClickable(downloadLink));
         downloadLink.click();
     }
 }
